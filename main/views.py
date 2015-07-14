a70eaeae9a2f74dffee547bffbd20aed156efee3
Alejandro Carlos <alexc@bixly.com>
2015-07-14 16:43:12 -0700

    Added base urls for home and inspire

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.views.generic.list import ListView
+from case_studies.models import CaseStudy
 
 
 class HomeView(ListView):
+    context_object_name = 'case_list'
+    model = CaseStudy
     template_name = 'index.html'
--- notch/urls.py
+++ notch/urls.py
@@ -19,8 +19,8 @@ from django.conf.urls import include, url, static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^inspire/', include('inspire.urls')),
-    # url(r'^/', include('main.urls')),
+    url(r'^$', include('main.urls')),
+    url(r'^inspire/', include('inspire.urls')),
     url(r'^case_study/', include('case_studies.urls')),
 ]
 
