73a6780191ca2f245543bad06fc25775c902f3fa
Alejandro Carlos <alexc@bixly.com>
2015-07-15 15:29:12 -0700

    Added contact form and urls

--- main/views.py
+++ main/views.py
@@ -1,8 +1,26 @@
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
 from django.views.generic.list import ListView
 from case_studies.models import CaseStudy
+from main.forms import ContactForm
 
 
 class HomeView(ListView):
     context_object_name = 'case_list'
     model = CaseStudy
     template_name = 'index.html'
+
+
+class ContactView(FormView):
+    template_name = 'contact.html'
+    form_class = ContactForm
+    success_url = '/contact/success'
+
+    def form_valid(self, form):
+        form.save()
+        # TODO Send contact form email
+        return super(ContactView, self).form_valid(form)
+
+
+class ContactSuccessView(TemplateView):
+    template_name = 'contact_success.html'
