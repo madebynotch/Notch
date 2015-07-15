81b8f19de022633059e051407582c0e7b5459b36
Rob Boettcher <robertb@bixly.com>
2015-07-15 09:37:24 -0700

    converted tabs to spaces

--- case_studies/views.py
+++ case_studies/views.py
@@ -4,8 +4,8 @@ from case_studies.models import CaseStudy, CaseStudyImage
 # Create your views here.
 
 def case_study(request, pk):
-	case_study = CaseStudy.objects.get(pk=pk)
+    case_study = CaseStudy.objects.get(pk=pk)
 
-	context = {'case_study': case_study,}
+    context = {'case_study': case_study,}
 
-	return render(request, 'case_study.html', context)
+    return render(request, 'case_study.html', context)
--- inspire/models.py
+++ inspire/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 class InspireItem(models.Model):
-	tag = models.ForeignKey('main.Tag', related_name='inspire_tags')
-	url = models.URLField()
-	image = models.ImageField(upload_to='inspire')
-	title = models.CharField(max_length=150)
-	description = models.TextField()
-	date_added = models.DateField(auto_now=True)
+    tag = models.ForeignKey('main.Tag', related_name='inspire_tags')
+    url = models.URLField()
+    image = models.ImageField(upload_to='inspire')
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    date_added = models.DateField(auto_now=True)
 
-	def __unicode__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
