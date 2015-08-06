85a527e004b8c3dd2e24ed899939ccd460c4c57d
Rob Boettcher <robertb@bixly.com>
2015-08-06 10:04:41 -0700

    added functionality for image resizing

--- inspire/models.py
+++ inspire/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+# from PIL import Image
+# import sys, time
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 
 class InspireItem(models.Model):
@@ -14,6 +18,10 @@ class InspireItem(models.Model):
                            choices=CATEGORY)
     url = models.URLField()
     image = models.ImageField(upload_to='inspire', blank=True)
+    resized_image = ImageSpecField(source='image',
+                                   processors=[ResizeToFill(640, 900)],
+                                   format='JPEG',
+                                   options={'quality': 100},)
     title = models.CharField(max_length=150)
     description = models.TextField()
     date_added = models.DateField(auto_now_add=True)
